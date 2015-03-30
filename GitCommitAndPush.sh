d1e64c3a234fed57fa2e00c518473e3bad487e76
Jacob Poston <jacobpsemail@yahoo.com>
2015-03-30 15:42:55 +0000

    Bash Logging Test

--- GitCommitAndPush.sh
+++ GitCommitAndPush.sh
@@ -1,5 +1,11 @@
 #!/bin/bash  
 git add .  
-read -p "Commit description: " desc  
+read -p "Commit description: " desc 
+
+current_time=$(date "+%Y.%m.%d-%H:%M:%S")
+
+echo "Git repository updated @ $current_time"
+echo [$current_time] - $desc >> updateLog.log
+
 git commit -m "$desc" 
 git push origin master
