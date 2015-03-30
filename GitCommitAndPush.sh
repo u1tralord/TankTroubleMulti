867486df306c999fa638e4754b827f7762f7ab24
Jacob Poston <jacobpsemail@yahoo.com>
2015-03-30 15:54:04 +0000

    Git bash scipt updated to make a local log of commits

--- GitCommitAndPush.sh
+++ GitCommitAndPush.sh
@@ -1,11 +1,11 @@
 #!/bin/bash  
-git add .  
 read -p "Commit description: " desc 
 
 current_time=$(date "+%Y.%m.%d-%H:%M:%S")
-
-echo "Git repository updated @ $current_time"
 echo [$current_time] - $desc >> updateLog.log
 
+git add .  
 git commit -m "$desc" 
 git push origin master
+
+echo "Git repository updated @ $current_time"
